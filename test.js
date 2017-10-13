7c19dfa8a6cb7630d7325b035cd3b613aa558075
Anthony Bois <boisan@iut1i-605-a01.local>
2017-10-13 15:23:18 +0200

    all characters  test.js files

--- test.js
+++ test.js
@@ -18,13 +18,17 @@ $( document ).ready(function() {
 
 	//liste des
 	$id=1;
-	$.getJSON( "https://swapi.co/api/people/", function( data ) {
-		jQuery.each(data.results, function()
-		{
-			$('#listeperso').append('<div class="col-md-4"><button type="button" class="btn btn-link" data-toggle="modal" data-target="#'+$id+'">'+this.name+'</button> </br></div>');
-			$('#descriptionperso').append('<div class="modal fade" id="'+$id+'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"> <div class="modal-dialog" role="document"><div class="modal-content"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="modal-body"><p><h3 class="modal-title" id="exampleModalLabel">'+this.name+'</h3></p><p>Anniversaire : '+this.birth_year+'</p><p>Année de naissance : '+this.birth_year+'</p><p>Couleur des yeux : '+this.eye_color+'</p><p>Genre : '+this.gender+'</p><p>Couleur de cheveux : '+this.hair_color+'</p><p>Taille : '+this.height+'</p><p>Poid : '+this.mass+'</p><p>Couleur de peau : '+this.skin_color+'</p></div></div></div></div>');
-			$id++;
+
+	for (i = 1; i<10; i++)
+	{
+		$.getJSON( "https://swapi.co/api/people/?page="+i, function( data ) {
+			jQuery.each(data.results, function()
+			{
+				$('#listeperso').append('<div class="col-md-4"><button type="button" class="btn btn-link" data-toggle="modal" data-target="#'+$id+'">'+this.name+'</button> </br></div>');
+				$('#descriptionperso').append('<div class="modal fade" id="'+$id+'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"> <div class="modal-dialog" role="document"><div class="modal-content"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="modal-body"><p><h3 class="modal-title" id="exampleModalLabel">'+this.name+'</h3></p><p>Anniversaire : '+this.birth_year+'</p><p>Année de naissance : '+this.birth_year+'</p><p>Couleur des yeux : '+this.eye_color+'</p><p>Genre : '+this.gender+'</p><p>Couleur de cheveux : '+this.hair_color+'</p><p>Taille : '+this.height+'</p><p>Poid : '+this.mass+'</p><p>Couleur de peau : '+this.skin_color+'</p></div></div></div></div>');
+				$id++;
+			});
 		});
-	});
+	}
 });
 
