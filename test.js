8441d62a370317b8c4d07b7476788583d6db14c6
Anthony Bois <boisan@iut1.local>
2017-10-13 11:58:32 +0200

    modify js file

--- test.js
+++ test.js
@@ -1,3 +1,18 @@
-$(".img-fluid").hover(function(){
+/*$(".img-fluid").hover(function(){
 	console.log($(this).attr("alt"));
+});*/
+
+
+$( document ).ready(function() {
+	$.getJSON( "https://swapi.co/api/people/", function( data ) {
+		jQuery.each(data.results, function()
+		{
+			console.log(this.name);
+			$('#listeperso').append("<p>"+this.name+"</p>");
+		});
+	})
+
+ 
+
 });
+
